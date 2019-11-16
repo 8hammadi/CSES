c8dd0ee763a952285336f756265cd731033c3701
Abdellatif-AitHammadi <57833816+Abdellatif-AitHammadi@users.noreply.github.com>
2019-11-16 14:23:56 +0100

    Create Bit_Strings.cpp
    https://cses.fi/problemset/task/1617

---
+++ Bit_Strings.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+const int max=1e9+7;
+long long c;
+int pow(int a,int n){
+	if(n==0) return 1;
+	else {
+		c=n/2;
+		c=pow(a,c)%max;
+		c=(c*c)%max;
+		c=(n%2)?c*a:c;
+		return c%max;
+}}
+int main(){
+	int n;
+	scanf("%d",&n);
+	return 0*printf("%d",pow(2,n));
+}
